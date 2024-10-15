55b189f125b84e29453f6d817507b630c19df042
João Ugulini <joaomiguelugulini@gmail.com>
2024-10-15 20:25:12 -0300

    Arrumei o HTML e o JS

--- js/principal.js
+++ js/principal.js
@@ -1,14 +1,16 @@
 const colores = ['red', 'blue', 'green', 'orange', 'purple'];
 
 function mudaColores() {
-    const elementos = document.querySelectorAll('.nome');
+    const elementos = document.querySelectorAll('.nome, button');
 
     elementos.forEach((element, index) => {
         let colorIndex = 0;
         setInterval(() => {
             element.style.color = colores[colorIndex];
+            element.style.backgroundColor = colores[(colorIndex + 1) % colores.length];
             colorIndex = (colorIndex + 1) % colores.length;
         }, 1000 * (index + 1));
     });
 }
+
 window.onload = mudaColores;
